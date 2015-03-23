269c23bdd89f59772fa6d8ede3413bb161331fd3
Sven Agneessens <sven.agneessens@gmail.com>
2015-03-23 14:48:42 +0100

    Initializing branch

---
+++ TaskMan/src/taskman/model/project/Project.java
@@ -0,0 +1,11 @@
+package taskman.model.project;
+
+import org.joda.time.DateTime;
+
+public class Project {
+	
+	public Project(String name, String description, DateTime creationTime, DateTime dueTime) {
+		
+	}
+
+}
