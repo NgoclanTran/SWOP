e133b73d5417e1dc49fd04da87aa841773e5de07
Ngoc-Lan <ngoclan.tran@studenet.kuleuven.be>
2015-03-31 20:54:11 +0200

    some tests

--- TaskMan/src/taskman/tests/TimeSpanTest.java
+++ TaskMan/src/taskman/tests/TimeSpanTest.java
@@ -6,23 +6,50 @@ import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 
+import taskman.exceptions.IllegalDateException;
 import taskman.model.project.task.TimeSpan;
 
 public class TimeSpanTest {
 	private DateTime start;
 	private DateTime end;
 
-@Before
-public void setup(){
-	start = new DateTime(2015,1,1,1,1);
-	end = new DateTime(2016,1,1,1,1);
-}
+	@Before
+	public void setup(){
+		start = new DateTime(2015,1,1,10,1);
+		end = new DateTime(2015,1,1,11,1);
+	}
 
-@Test
-public void constructorTest_TrueCase(){
-	TimeSpan ts = new TimeSpan(start, end);
-	assertEquals(ts.getStartTime(), start);
-	assertEquals(ts.getEndTime(), end);
-}
+	@Test
+	public void constructorTest_TrueCase(){
+		TimeSpan ts = new TimeSpan(start, end);
+		assertEquals(ts.getStartTime(), start);
+		assertEquals(ts.getEndTime(), end);
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void constructorTest_FalseCase_StartEndTime(){
+		DateTime start = new DateTime(2015,1,1,12,1);
+		DateTime end = new DateTime(2014,1,1,12,1);
+		TimeSpan ts = new TimeSpan(start, end);
+	}
+
+	@Test (expected = IllegalDateException.class)
+	public void constructorTest_FalseCase_StartTime(){
+		DateTime start = new DateTime(2015,1,1,1,1);
+		TimeSpan ts = new TimeSpan(start, end);
+	}
+	
+	@Test (expected = IllegalDateException.class)
+	public void constructorTest_FalseCase_EndTime(){
+		DateTime t = new DateTime(2016,1,1,20,1);
+		TimeSpan ts = new TimeSpan(start, t);
+		
+	}
+	
+	@Test
+	public void calculatedPerformedTimeTest_TrueCase(){
+		TimeSpan ts = new TimeSpan(start, end);
+		assertEquals(ts.calculatePerformedTime(),60);
+	}
 
 }
