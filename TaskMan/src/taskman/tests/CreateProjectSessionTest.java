66a4b94111f1830cdd2c1841de6c45bea25bf75e
Ngoc-Lan <ngoclan.tran@studenet.kuleuven.be>
2015-04-22 00:52:48 +0200

    creatProject usecase scenario TODO: last succes scenario

--- TaskMan/src/taskman/tests/CreateProjectSessionTest.java
+++ TaskMan/src/taskman/tests/CreateProjectSessionTest.java
@@ -2,98 +2,93 @@ package taskman.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
 import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.*;
+
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
 import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 import org.mockito.Mockito;
 
 import taskman.controller.MainSession;
 import taskman.controller.project.CreateProjectSession;
+import taskman.controller.project.CreateTaskSession;
 import taskman.model.ProjectHandler;
 import taskman.model.project.Project;
+import taskman.model.project.task.Task;
 import taskman.view.IView;
 import taskman.view.View;
 
 public class CreateProjectSessionTest {
-	@Rule
-	public TextFromStandardInputStream systemInMock = emptyStandardInputStream();
-
 	private IView cli;
-	private CreateProjectSession createProject;
 	private ProjectHandler ph;
+	private CreateProjectSession session;
 	private String description;
-	private String name;
-	private DateTime dueTime;
+	private ArrayList<Task> dependencies;
+	private Project p;
+
+	@Rule
+	public final TextFromStandardInputStream systemInMock = emptyStandardInputStream();
+
+	@Rule
+	public final StandardOutputStreamLog log = new StandardOutputStreamLog();
 
 	@Before
 	public void setup() {
-		cli = Mockito.mock(View.class);
+		cli = new View();
 		ph = new ProjectHandler();
-		createProject = new CreateProjectSession(cli, ph);
-		ph = new ProjectHandler();
-		MainSession controller = new MainSession(cli, ph);
-		createProject = new CreateProjectSession(cli, ph);
+		session = new CreateProjectSession(cli, ph);
+
+	}
+
+//	@Test
+//	public void useCaseTest_Name_Cancelcreation(){
+//		// -------- Before running ----------
+//		ArrayList<Project> projects = (ArrayList<Project>) ph.getProjects();
+//
+//		systemInMock.provideText("cancel");
+//		session.run();
+//		assertEquals(ph.getProjects(), projects);
+//		String lastMessage = log.getLog().substring(0, 42);
+//		assertEquals("Enter the name of the project (or cancel):", lastMessage);
+//
+//	}
+
+	@Test
+	public void useCaseTest_Description_Cancelcreation(){
+		// -------- Before running ----------
+		ArrayList<Project> projects = (ArrayList<Project>) ph.getProjects();
+
+		systemInMock.provideText("name\ncancel");
+		session.run();
+		
+		// --------- Check if nothing has changed or created --------------
+		assertEquals(ph.getProjects(), projects);
+		assertEquals("Enter the name of the project (or cancel):\r\n\r\n\r\nEnter the description of the project (or cancel):\r\n\r\n\r\n", log.getLog());
+
+	}
+	
+	@Test 
+	public void useCaseTest_DueTime_CancelCreation(){
+		// -------- Before running ----------
+		ArrayList<Project> projects = (ArrayList<Project>) ph.getProjects();
+
+		systemInMock.provideText("name\n21-04-201511:11\n21-04-2015 10:10\ncancel");
+		session.run();
+		
+		// --------- Check if nothing has changed or created --------------
+		assertEquals(ph.getProjects(), projects);
+		assertEquals("Enter the name of the project (or cancel):\r\n\r\n\r\nEnter the description of the project (or cancel):\r\n\r\n\r\nEnter the due time of the project with format dd-MM-yyyy HH:mm (or cancel):\r\n\r\n\r\nDue time has to be after creation time.\r\n\r\nEnter the name of the project (or cancel):\r\n\r\n\r\n", log.getLog());
+	}
+	
+	@Test
+	public void useCaseTest_SuccesScenario(){
 		
 	}
 
-	@Test
-	public void testRun() {
-		DateTime creationTime = new DateTime();
-		DateTime dueTime = new DateTime();
-		createProject.getPH().addProject("test", "", creationTime, dueTime);
-		assertEquals("test", createProject.getPH().getProjects().get(0).getName());
-		assertEquals("", createProject.getPH().getProjects().get(0).getDescription());
-		assertEquals(creationTime, createProject.getPH().getProjects().get(0).getCreationTime());
-		assertEquals(dueTime, createProject.getPH().getProjects().get(0).getDueTime());
-
-	}
-	@Test
-	public void useCase_SuccesScenario(){
-
-		Mockito.when(cli.getNewProjectName()).thenReturn(name);
-		Mockito.when(cli.getNewProjectDescription()).thenReturn(description);
-		Mockito.when(cli.getNewProjectDueTime()).thenReturn(dueTime);
-
-		createProject.run();
-
-		// Last task in project is the new task
-		Project p = ph.getProjects().get(ph.getProjects().size()-1);
-		assertEquals(p.getDescription(),description);
-		assertEquals(p.getName(),name);
-		assertEquals(p.getDueTime(), dueTime);
-
-
-	}
-	// zal blijven lopen, hoe testen?
-//	@Test(expected = IllegalArgumentException.class)
-//	public void useCase_FailIncorrectDueTimeNull(){
-//		Mockito.when(cli.getNewProjectName()).thenReturn(name);
-//		Mockito.when(cli.getNewProjectDescription()).thenReturn(description);
-//		Mockito.when(cli.getNewProjectDueTime()).thenReturn(null);
-//
-//		createProject.run();
-//		
-//	}
-//	@Test(expected = IllegalArgumentException.class)
-//	public void useCase_FailIncorrectNameNull(){
-//		Mockito.when(cli.getNewProjectName()).thenReturn(null);
-//		Mockito.when(cli.getNewProjectDescription()).thenReturn(description);
-//		Mockito.when(cli.getNewProjectDueTime()).thenReturn(dueTime);
-//
-//		createProject.run();
-//		
-//	}
-//	@Test(expected = IllegalArgumentException.class)
-//	public void useCase_FailIncorrectDescriptionNull(){
-//		Mockito.when(cli.getNewProjectName()).thenReturn(name);
-//		Mockito.when(cli.getNewProjectDescription()).thenReturn(null);
-//		Mockito.when(cli.getNewProjectDueTime()).thenReturn(dueTime);
-//
-//		createProject.run();
-//		
-//	}
-	
 }
