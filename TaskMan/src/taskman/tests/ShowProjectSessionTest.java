b1712a6b4fb82a4b6f2c076dba6a18997c382610
Raf Ponsaerts <raf.ponsaerts@student.kuleuven.be>
2015-04-27 02:47:18 +0200

    TODO: Fout in het parsen van de log van show project, vind de oplossing niet direct.

--- TaskMan/src/taskman/tests/ShowProjectSessionTest.java
+++ TaskMan/src/taskman/tests/ShowProjectSessionTest.java
@@ -19,6 +19,7 @@ import org.mockito.Mockito;
 import taskman.controller.project.CreateProjectSession;
 import taskman.controller.project.ShowProjectSession;
 import taskman.model.ProjectHandler;
+import taskman.model.ResourceHandler;
 import taskman.model.project.Project;
 import taskman.model.project.task.Task;
 import taskman.view.IView;
@@ -27,6 +28,7 @@ import taskman.view.View;
 public class ShowProjectSessionTest {
 	private IView cli,cli1;
 	private ProjectHandler ph, ph1;
+	private ResourceHandler rh;
 	private ShowProjectSession session, session1;
 
 	@Rule
@@ -41,7 +43,8 @@ public class ShowProjectSessionTest {
 		// Session with projects
 		cli = new View();
 		ph = new ProjectHandler();
-		session = new ShowProjectSession(cli, ph);
+		rh = new ResourceHandler();
+		session = new ShowProjectSession(cli, ph, rh);
 		ph.addProject("Project x", "Test project 1", new DateTime(), new DateTime(2016, 4, 1, 0, 0));
 		ph.addProject("Project y", "Test project 2", new DateTime(), new DateTime(2016, 4, 1, 0, 0));
 		ph.getProjects().get(0).addTask("Task description", 10, 1, new ArrayList<Task>(), null);
@@ -50,7 +53,7 @@ public class ShowProjectSessionTest {
 		// Session without projects
 		cli1 = new View();
 		ph1 = new ProjectHandler();
-		session1 = new ShowProjectSession(cli1, ph1);
+		session1 = new ShowProjectSession(cli1, ph1, rh);
 		
 	}
 	
@@ -63,7 +66,7 @@ public class ShowProjectSessionTest {
 	public void useCaseTest_WithProjects(){
 		systemInMock.provideText("1\n1");
 		session.run();
-		assertEquals(log.getLog().substring(286),"Task:\r\n\tTask description\r\n\tStatus: AVAILABLE\r\n\r\n\r\n");
+		assertEquals(log.getLog().substring(286),"Task:\r\n\tTask description\r\n\tStatus: AVAILABLE\r\n\tAcceptable deviation: 1 %\r\r\n\r\n\r\n");
 	}
 
 
