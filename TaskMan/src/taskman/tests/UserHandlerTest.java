2a696a163df34cfa5b9ca5c8cc85ad921a913a97
Ngoc-Lan <ngoclan.tran@studenet.kuleuven.be>
2015-04-30 11:50:27 +0200

    userhandler junt

---
+++ TaskMan/src/taskman/tests/UserHandlerTest.java
@@ -0,0 +1,23 @@
+package taskman.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import taskman.model.UserHandler;
+
+public class UserHandlerTest {
+
+	private UserHandler u;
+	@Before
+	public void setUp() throws Exception {
+		u = new UserHandler();
+	}
+
+	@Test
+	public void getDevelopersTest(){
+		List<Developer> d = u.get
+	}
+
+}
