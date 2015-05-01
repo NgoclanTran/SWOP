1783fea963832eb81dc66b5c4a3c1d8610acae1a
Sven <sven.agneessens@gmail.com>
2015-05-01 17:28:35 +0200

    Remove reservations at conflict.

--- TaskMan/src/taskman/view/IUpdateTaskForm.java
+++ TaskMan/src/taskman/view/IUpdateTaskForm.java
@@ -25,10 +25,33 @@ public interface IUpdateTaskForm {
 	public Developer getDeveloper(List<Developer> developers)
 			throws ShouldExitException;
 
+	/**
+	 * This method will ask the user to enter whether or not the selected task
+	 * has failed.
+	 */
 	public boolean getUpdateTaskFailed();
 
+	/**
+	 * This method will view the start time of the selected task and ask the
+	 * user to enter the end time of the selected task. Afterwards it will
+	 * return the end time as a DateTime.
+	 * 
+	 * @param startTime
+	 * 
+	 * @return Returns the end time as a DateTime.
+	 * 
+	 * @throws ShouldExitException
+	 */
 	public DateTime getUpdateTaskStopTime(DateTime startTime);
 
+	/**
+	 * This method will ask the user to select a project and returns it.
+	 * 
+	 * @param projects
+	 * @param availableTasks
+	 * 
+	 * @throws ShouldExitException
+	 */
 	public Project getProjectWithAvailableTasks(List<Project> projects,
 			List<List<Task>> availableTasks);
 
