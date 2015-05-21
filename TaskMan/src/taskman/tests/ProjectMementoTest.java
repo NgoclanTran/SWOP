ab12e4e701205efb13e8b2f00213c6dffbb90cfa
Raf Ponsaerts <raf.ponsaerts@student.kuleuven.be>
2015-05-22 00:09:42 +0200

    Fixed ClockMementoTest and ProjectMementoTests

--- TaskMan/src/taskman/tests/ProjectMementoTest.java
+++ TaskMan/src/taskman/tests/ProjectMementoTest.java
@@ -8,33 +8,48 @@ import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 
+import taskman.model.company.BranchOffice;
+import taskman.model.company.Company;
+import taskman.model.memento.ClockMemento;
 import taskman.model.memento.ProjectMemento;
 import taskman.model.project.Project;
+import taskman.model.task.NormalTask;
 import taskman.model.task.Task;
 import taskman.model.task.TaskFactory;
 import taskman.model.task.Task;
 import taskman.model.time.Clock;
 
 public class ProjectMementoTest {
-	private ArrayList<Task> tasks = new ArrayList<Task>();
-	private String stateName;
+	private ArrayList<NormalTask> tasks = new ArrayList<NormalTask>();
+	private String stateName = "";
 	private ProjectMemento p;
 	private Clock clock;
+	private Company company;
+	private BranchOffice branchOffice = new BranchOffice(company, "", null);
+	private ProjectMemento pm;
 		
 	@Before
 	public void setup(){
 		clock = new Clock();
-		Task t = new Task(clock,"",10,10,null,null,null);
-		tasks.add(t);
-		Project project = new Project("name", "description", new DateTime(2015,10,12,10,10), new DateTime(2015,10,12,12,10), new TaskFactory(clock));
-		p = new ProjectMemento(project, tasks, stateName);
+		branchOffice.getPh().addProject("", "", new DateTime(), new DateTime());
+		Project p = branchOffice.getPh().getProjects().get(0);
+		p.addTask("", 10, 1, null, null, null);
+		tasks.add(p.getTasks().get(0));
+		Project project = new Project("name", "description", new DateTime(2015,10,12,10,10), new DateTime(2015,10,12,12,10), new TaskFactory(branchOffice, clock));
+		pm = new ProjectMemento(project, tasks, stateName);
 	}
 	@Test
 	public void stateNameTest(){
-		assertEquals(p.getStateName(), stateName);
+		assertEquals(pm.getStateName(), stateName);
 	}
 	@Test
 	public void tasksTest(){
-		assertEquals(p.getTasks(), tasks);
+		assertEquals(pm.getTasks(), tasks);
+	}
+	@Test
+	public void getObjectTest(){
+		Project project = new Project("name", "description", new DateTime(2015,10,12,10,10), new DateTime(2015,10,12,12,10), new TaskFactory(branchOffice, clock));
+		ProjectMemento p1= new ProjectMemento(project, tasks, stateName);
+		assertEquals(p1.getObject(), project);
 	}
 }
