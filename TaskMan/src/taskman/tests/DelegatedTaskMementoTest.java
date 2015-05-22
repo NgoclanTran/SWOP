25efd2bdf9dd259304e49c54163cb08c5ffb94b1
Ngoc-Lan <ngoclan.tran@studenet.kuleuven.be>
2015-05-22 02:03:57 +0200

    junit

--- TaskMan/src/taskman/tests/DelegatedTaskMementoTest.java
+++ TaskMan/src/taskman/tests/DelegatedTaskMementoTest.java
@@ -1,15 +1,19 @@
 package taskman.tests;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 
 import org.joda.time.DateTime;
 import org.junit.Before;
+import org.junit.Test;
 
 import taskman.model.company.BranchOffice;
 import taskman.model.company.Company;
 import taskman.model.memento.DelegatedTaskMemento;
 import taskman.model.memento.NormalTaskMemento;
 import taskman.model.project.Project;
+import taskman.model.task.DelegatedTask;
 import taskman.model.task.NormalTask;
 import taskman.model.time.Clock;
 import taskman.model.time.TimeSpan;
@@ -30,7 +34,20 @@ public class DelegatedTaskMementoTest {
 		branchOffice.getPh().addProject("", "", new DateTime(), new DateTime());
 		Project p = branchOffice.getPh().getProjects().get(0);
 		p.addTask("", 10, 1, null, null, null);
-		DelegatedTaskMemento dtm = new DelegatedTaskMemento(branchOffice.getPh().getProjects().get(0).getTasks().get(0), true, "", timeSpan)
+		DelegatedTask delegatedTask = new DelegatedTask(clock, "description", 10, 1, null, true);
+		DelegatedTaskMemento dtm = new DelegatedTaskMemento(delegatedTask, true, "name", new TimeSpan(new DateTime(2015,10,12,10,0), new DateTime(2015,10,12,14,0)));
+		
+	}
+	
+	@Test
+	public void constructorTest(){
+		DelegatedTask delegatedTask = new DelegatedTask(clock, "description", 10, 1, null, true);
+		TimeSpan ts = new TimeSpan(new DateTime(2015,10,12,10,0), new DateTime(2015,10,12,14,0));
+		DelegatedTaskMemento dtm = new DelegatedTaskMemento(delegatedTask, true, "name", ts );
+		assertTrue(dtm.getDependenciesFinished());
+		assertEquals(dtm.getStateName(), "name");
+		assertEquals(dtm.getTimeSpan(), ts);
+		assertEquals(dtm.getObject(), delegatedTask);
 	}
 	
 	
