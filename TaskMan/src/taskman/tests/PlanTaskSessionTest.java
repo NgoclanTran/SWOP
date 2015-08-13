3d79448fc40b913601847b12320716a38fc91ef0
Michael Claes <michaelc1992@hotmail.com>
2015-08-13 18:37:40 +0200

    Plan task custom date test

--- TaskMan/src/taskman/tests/PlanTaskSessionTest.java
+++ TaskMan/src/taskman/tests/PlanTaskSessionTest.java
@@ -16,6 +16,7 @@ import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 
 import taskman.TaskMan;
 import taskman.controller.branch.PlanTaskSession;
+import taskman.exceptions.ShouldExitException;
 import taskman.model.company.BranchOffice;
 import taskman.model.company.Company;
 import taskman.model.company.ProjectHandler;
@@ -35,39 +36,37 @@ public class PlanTaskSessionTest {
 	@Rule
 	public final StandardOutputStreamLog log = new StandardOutputStreamLog();
 
-
-
 	@Test
 	public void useCaseTest_Succes() {
 
-		//Deze test plan de eerste taak van project Y
+		// Deze test plan de eerste taak van project Y
 		// - plan task
 		// - show task
-		systemInMock.provideText("3\n1\n1\n1\n4\n2\n1\n2\n1\nn\n8\n2");
+		systemInMock.provideText("3\n1\n1\n1\n4\n2\n1\n2\n1\nN\n8\n2");
 		TaskMan.main(null);
 
 		// Check of de taak gepland is
-		assertTrue( log.getLog().contains("Task planned."));
+		assertTrue(log.getLog().contains("Task planned."));
 	}
 
-//	@Test
-//	public void useCaseTest_Succes_Input_StartTime(){
-//		//Deze test plan de tweede taak van project Y
-//		// - plan task
-//		// - show task
-//		systemInMock.provideText("1\n1\n1\n4\n2\n1\n4\n02-04-2014 09:00\n5\nN\n1\n1\n2\n1\n8\n2\n");
-//		TaskMan.main(null);
-//
-//
-//		// Check of de taak gepland is
-//		assertTrue( log.getLog().contains("Task planned."));
-//
-//		// Check of de status van die taak PLANNED is
-//		assertTrue(log.getLog().contains("Description: another task description\n\tStatus: PLANNED"));
-//		//TODO nog de tijd checken
-//	}
+	 @Test
+	 public void useCaseTest_Succes_Input_StartTime(){
+	 //Deze test plan de tweede taak van project Y
+	 // - plan task
+	 // - show task
+	 systemInMock.provideText("1\n1\n1\n1\n4\n2\n1\n4\n02-04-2014 09:00\n2\nN\n1\n1\n2\n1\n8\n2\n");
+	 TaskMan.main(null);
+	
+	
+	 // Check of de taak gepland is
+	 assertTrue( log.getLog().contains("Task planned."));
+	
+	 // Check of de status van die taak PLANNED is
+	 assertTrue(log.getLog().contains("Task planned."));
+	 //TODO nog de tijd checken
+	 }
 
-// TODO test conflic resource
-// TODO test conflict developper
+	// TODO test conflic resource
+	// TODO test conflict developper
 
 }
