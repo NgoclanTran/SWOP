8bfea199bbc2400bfc4ab9f4e346914ad9d8725e
Michael Claes <michaelc1992@hotmail.com>
2015-08-14 13:39:24 +0200

    New Test

--- TaskMan/src/taskman/tests/TaskManTest.java
+++ TaskMan/src/taskman/tests/TaskManTest.java
@@ -18,12 +18,10 @@ public class TaskManTest {
 	@Rule
 	public final StandardOutputStreamLog log = new StandardOutputStreamLog();
 
-//	@Test
-//	public void testMain() {
-//		systemInMock.provideText("5");
-//		TaskMan.main(null);
-//		assertEquals(
-//				"TaskMan V2.0\r\n\r\n\r\n1. Show projects\r\n2. Create project\r\n3. Create task\r\n4. Update task\r\n5. Quit\r\n\r\nSelect an option:\r\n\r\n\r\n",
-//				log.getLog());
-//	}
+	@Test
+	public void testMain() {
+		systemInMock.provideText("3\n1\n1\n1\n8\n2\n");
+		TaskMan.main(null);
+		assertTrue(log.getLog().contains("Main menu"));
+	}
 }
